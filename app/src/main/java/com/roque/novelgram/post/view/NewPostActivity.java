12c8b271f0fa1ce44d2ed1e3d118c02564e001ae
Roque Alarcon <roquealarcon363@gmail.com>
2019-03-26 09:14:52 -0500

    add storage foto in firebase

--- app/src/main/java/com/roque/novelgram/post/view/NewPostActivity.java
+++ app/src/main/java/com/roque/novelgram/post/view/NewPostActivity.java
@@ -2,20 +2,24 @@ package com.roque.novelgram.post.view;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -24,14 +28,18 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 import com.roque.novelgram.NovelgramApplication;
 import com.roque.novelgram.R;
+import com.roque.novelgram.model.Picture;
 import com.squareup.picasso.Picasso;
 
 import java.io.ByteArrayOutputStream;
@@ -39,6 +47,8 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class NewPostActivity extends AppCompatActivity {
@@ -56,10 +66,14 @@ public class NewPostActivity extends AppCompatActivity {
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final int REQUEST_CAMERA = 0;
     private String photoPathTemp = "";
+    private String photoName = "";
 
     private NovelgramApplication app;
     private StorageReference storageReference;
     private FirebaseFirestore firebaseFirestore;
+
+    private UploadTask uploadTask;
+    private StorageTask storageTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,8 +82,10 @@ public class NewPostActivity extends AppCompatActivity {
         showToolbar("Crear Nuevo Post", true);
 
         app = (NovelgramApplication) getApplicationContext();
+        // [START get_firestore_instance]
         storageReference = app.getStorageReference();
         firebaseFirestore = FirebaseFirestore.getInstance();
+        // [END get_firestore_instance]
 
         imgPhoto = (ImageView)findViewById(R.id.imgPhoto);
         btnImgContent = (LinearLayout)findViewById(R.id.btnImgContent);
@@ -104,10 +120,17 @@ public class NewPostActivity extends AppCompatActivity {
         btnCreatePost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                uploadPhoto();
+                if(!validatePost()){
+                    return;
+                }
+                if (uploadTask != null && uploadTask.isInProgress() ) {
+                    Toast.makeText(NewPostActivity.this,"Subiendo foto", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadFile();
+                }
+                //uploadPhoto();
             }
         });
-
     }
 
     private void selectOption() {
@@ -143,33 +166,135 @@ public class NewPostActivity extends AppCompatActivity {
 
         Bitmap bitmap = imgPhoto.getDrawingCache();
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
 
         byte[] photoByte = byteArrayOutputStream.toByteArray();
-        String photoName = photoPathTemp.substring(photoPathTemp.lastIndexOf("/")+1, photoPathTemp.length());
 
         final StorageReference photoReference = storageReference.child("postImages/" + photoName);
 
         UploadTask uploadTask = photoReference.putBytes(photoByte);
-        uploadTask.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w(TAG, "Error al subir la foto " + e.toString());
-                e.printStackTrace();
-            }
-        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                photoReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        uploadTask
+                .addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onSuccess(Uri uriPhoto) {
-                        String photoURL = uriPhoto.toString();
-                        Log.w(TAG, "URL Photo > " + photoURL);
-                        finish();
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error al subir la foto " + e.toString());
+                        e.printStackTrace();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        progressBar.setProgress((int) progress);
+                    }
+                })
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        photoReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uriPhoto) {
+                                String photoURL = uriPhoto.toString();
+                                String timeStamp = new SimpleDateFormat("yyMMdd_HH-mm-ss").format(new Date());
+                                String title = txtTitle.getText().toString().trim();
+                                Log.w(TAG, "URL Photo > " + photoURL);
+
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        progressBar.setProgress(0);
+                                    }
+                                },500);
+                                Picture picture = new Picture(photoURL, title, timeStamp,"0");
+                                firebaseFirestore.collection("pictures").add(picture);
+
+                                Toast.makeText(NewPostActivity.this,"Subida exitosa", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        });
                     }
                 });
-            }
-        });
+    }
+
+    private boolean validatePost() {
+        boolean valid = true;
+        String title = txtTitle.getText().toString();
+        imgPhoto.setDrawingCacheEnabled(true);
+        imgPhoto.buildDrawingCache();
+        Bitmap bitmap = imgPhoto.getDrawingCache();
+
+        if (TextUtils.isEmpty(title)) {
+            txtTitle.setError("Campo requerido");
+            valid = false;
+        } else {
+            txtTitle.setError(null);
+        }
+
+        if (bitmap == null) {
+            valid = false;
+            Toast.makeText(this,"Ningún archivo seleccionado",Toast.LENGTH_SHORT).show();
+        }
+
+        return valid;
+    }
+
+    // Get the file extension from de image
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (mImageUri != null) {
+            final StorageReference fileReference = storageReference.child("postImages/" + System.currentTimeMillis()+"."+getFileExtension(mImageUri));
+            final String timeStamp = new SimpleDateFormat("yyMMdd_HH-mm-ss").format(new Date());
+            uploadTask = fileReference.putFile(mImageUri);
+
+            uploadTask
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(NewPostActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressBar.setProgress((int) progress);
+                        }
+                    })
+                    .continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()){
+                                throw Objects.requireNonNull(task.getException());
+                            }
+                            return fileReference.getDownloadUrl();
+                        }
+                    })
+                    .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(@NonNull Uri downloadUri) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            },500);
+                            Toast.makeText(NewPostActivity.this,"Subida exitosa", Toast.LENGTH_SHORT).show();
+
+                            Picture picture = new Picture(downloadUri.toString(),txtTitle.getText().toString().trim(),timeStamp,"0");
+                            firebaseFirestore.collection("pictures").add(picture);
+
+                        }
+                    });
+        } else {
+            Toast.makeText(this,"Ningún archivo seleccionado",Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void openFileChooser() {
@@ -177,8 +302,6 @@ public class NewPostActivity extends AppCompatActivity {
                 Intent.ACTION_PICK,
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         );
-        //pickPhoto.setType("image/*");
-        //pickPhoto.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(pickPhoto, PICK_IMAGE_REQUEST);
     }
 
@@ -192,12 +315,16 @@ public class NewPostActivity extends AppCompatActivity {
                 case 0:
                     Log.d(TAG, "CAMERA OK!! :");
                     hideImageBackground();
+
+                    photoName = photoPathTemp.substring(photoPathTemp.lastIndexOf("/")+1, photoPathTemp.length());
                     Toast.makeText(NewPostActivity.this, messageLoading, Toast.LENGTH_SHORT).show();
                     Picasso.get().load(photoPathTemp).into(imgPhoto);
                     break;
                 case 1:
                     mImageUri = data.getData();
                     hideImageBackground();
+
+                    photoName = System.currentTimeMillis()+"."+getFileExtension(mImageUri);
                     Toast.makeText(NewPostActivity.this, messageLoading, Toast.LENGTH_SHORT).show();
                     Picasso.get().load(mImageUri).into(imgPhoto);
                     break;
